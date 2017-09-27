c3c2af29fa9d6c4ece45f98e1aea3228a5a6898c
peterjriser <31518624+peterjriser@users.noreply.github.com>
2017-09-27 15:31:43 -0600

    Add files via upload

---
+++ guess_my_number.py
@@ -0,0 +1,26 @@
+print("I'm going to guess your number between 1 and 100, if my guess is correct, type 'Correct'")
+print("if my guess is two high type 'High' ")
+print("If it's less than, press 'Low'")
+
+lower_bound = 0
+upper_bound = 100
+number = int((upper_bound - lower_bound)/2)
+done = False
+tries = 0
+guess = input(number)
+
+while done != True:
+	if guess == "High":
+		upper_bound = number
+		number = int((upper_bound - lower_bound)/2)
+		guess = input(number)
+	elif guess == "Low":
+		lower_bound = number
+		number = int((upper_bound - lower_bound)/2)
+	        guess = input(number)			
+	elif guess == "Correct":
+		print("I got it! And in only", tries, " tries")
+		done = True
+	tries += 1
+print("See, the house always wins!!")
+
